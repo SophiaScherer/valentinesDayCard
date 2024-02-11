f6194c5b8c2608433ee1a22c3f65a6160d2aa241
SophiaScherer <Sophiasch@gmail.com>
2024-02-10 18:48:49 -0800

    initial files

---
+++ buttonText.js
@@ -0,0 +1,24 @@
+const buttonStatements = [
+    "test1",
+    "test2",
+    "test3",
+    "test4",
+    "test5",
+];
+
+function changeText() {
+    const newText = buttonStatements[Math.floor(Math.random()*buttonStatements.length)];
+    document.getElementById("newMessage").childNodes[0].nodeValue = newText;
+}
+
+function increaseFontSize() {
+    let txt = document.getElementById("yes");
+    let style = window.getComputedStyle(txt, null).getPropertyValue("font-size");
+    let currentSize = parseFloat(style);
+    txt.style.fontSize = (currentSize * 1.5) + "px";
+}
+
+function show() {
+    document.getElementById("yesImage").style.display = "block";
+    document.getElementById("yesButton").style.display = "hidden";
+}
